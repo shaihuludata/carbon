b60332433b7121acba3852d58c9328eb40955701
shaihuludata <sandworm1986@gmail.com>
2018-08-28 01:33:04 +0700

    Lesson 3 on the go

---
+++ app/routes.py
@@ -0,0 +1,46 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+# ...
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Denis'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+@app.route('/bugoga')
+def bugoga():
+    return 'bugogashenka'
+
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from app import app
-app.run(debug = True)
+
+app.run(debug=True)
 
 #ye
